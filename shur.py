2de10d1e4fad44837efc143c07d2760f9ec9d1e9
Ganyuk <mamontant2@yandex.com>
2026-02-22 20:03:26 +0300

    add print(word_sh)

--- shur.py
+++ shur.py
@@ -1,4 +1,5 @@
 import random
+import def_print_name
 import time
 
 word = ""
@@ -8,8 +9,11 @@ count_char = int(input("Number of random characters:"))
 start = time.time()
 for i in range(count_char*8):
     word_sh += str(random.randint(0,1))
+    print(word_sh)
 end = time.time()
 print(word_sh)
+def_print_name.print_name()
+#print time
 print(f"time: {end-start}")
 normal_string = ''.join(chr(int(word_sh[i:i+8], 2)) for i in range(0, len(word_sh), 8))
 print(normal_string)
