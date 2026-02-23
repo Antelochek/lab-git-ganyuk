65478af9921a24cfb93a714ad9349f92e1c079c4
cherseroff <proffitm1nd@gmail.com>
2026-02-23 14:12:17 +0300

    Some refactoring of code of mine friend

--- shur.py
+++ shur.py
@@ -1,22 +1,21 @@
 import random
-import def_print_name
 import time
 
-word = ""
-word_sh = ""
-ch = ""
-count_char = int(input("Number of random characters:"))
+count_char = int(input("Number of random characters: "))
 start = time.time()
-for i in range(count_char*8):
-    word_sh += str(random.randint(0,1))
-    print(word_sh)
-end = time.time()
-print(word_sh)
-def_print_name.print_name()
-<<<<<<< HEAD
-#print time
-=======
->>>>>>> e3b76d5cd65c110c63deca67c75464d3c2f08890
-print(f"time: {end-start}")
+
+# Генерация случайной двоичной строки
+word_sh = ""
+for i in range(count_char * 8):
+    word_sh += str(random.randint(0, 1))
+
+# Преобразование в текст
 normal_string = ''.join(chr(int(word_sh[i:i+8], 2)) for i in range(0, len(word_sh), 8))
-print(normal_string)
+
+end = time.time()
+
+print(f"Binary: {word_sh}")
+print("╔" + "═"*20 + "╗")
+print(f"║ {normal_string:^18} ║")
+print("╚" + "═"*20 + "╝")
+print(f"Time: {end-start} seconds")
