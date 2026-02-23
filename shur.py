b99b3e8631a87ce2898a2411e661b23966a5f5b6
Ganyuk <mamontant2@yandex.com>
2026-02-23 14:31:11 +0300

    Save current state before revert

--- shur.py
+++ shur.py
@@ -13,10 +13,7 @@ for i in range(count_char*8):
 end = time.time()
 print(word_sh)
 def_print_name.print_name()
-<<<<<<< HEAD
 #print time
-=======
->>>>>>> e3b76d5cd65c110c63deca67c75464d3c2f08890
 print(f"time: {end-start}")
 normal_string = ''.join(chr(int(word_sh[i:i+8], 2)) for i in range(0, len(word_sh), 8))
 print(normal_string)
